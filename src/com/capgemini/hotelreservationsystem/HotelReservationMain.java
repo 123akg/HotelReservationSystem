61fed3a227a40721bbbe9de60df1481c66ea717f
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-14 10:25:37 +0530

    Welcome to Hotel Reservation System

---
+++ src/com/capgemini/hotelreservationsystem/HotelReservationMain.java
@@ -0,0 +1,12 @@
+package com.capgemini.hotelreservationsystem;
+
+import java.util.Scanner;
+
+public class HotelReservationMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Hotel Reservation Program");
+
+	}
+}
